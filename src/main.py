be930b3a6f23172d51634a87ce1a1c6f31c108e4
Camilo Pineda <b.camilo.15@gmail.com>
2025-06-08 07:09:26 -0300

    Refactor main function to utilize ReportService for sales reporting and analysis

--- src/main.py
+++ src/main.py
@@ -1,33 +1,18 @@
 from src.database.connection import DatabaseConnection
 from sqlalchemy import text
 import pandas as pd
+from src.services.report_service import ReportService
+
 
 def main():
     try:
         db = DatabaseConnection()
-        #session = db.get_session()
         
-        # result = session.execute(text("SELECT COUNT(*) FROM customers"))
-        # count = result.scalar()
-        # print(f"Número de clientes en la base de datos: {count}")
-
-        # result = session.execute(text("SELECT * FROM categories"))
-        # rows = result.fetchall()
-        # print(f"Categorias:")
-        # for row in rows:
-        #     print(f"- ID: {row[0]}, Nombre: {row[1]}")
-
-        # result = session.execute(text("SELECT * FROM categories"))
-        # df = pd.DataFrame(result.fetchall(), columns=result.keys())
-        # print(df)
-
-        # db.close_session(session)
-
-        query = "SELECT * FROM employees"
-        df = db.execute_query_as_dataframe(query)
-        print("Resultados de la consulta:")
-        print(df)
-
+        report = ReportService(db)
+        report.sales_by_branch_and_hour()
+        report.customer_behavior()
+        report.product_performance(3)
+                
     except Exception as e:
         print("Error al conectar a la base de datos:", e)
 
